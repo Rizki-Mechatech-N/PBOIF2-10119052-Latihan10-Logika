9cfd63948239c4e1a4b01f8fc565d4e06d271ce6
Kiki <Kiki@Kiki-PC>
2020-10-18 11:24:43 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan10/logika/PBOIF210119052Latihan10Logika.java
@@ -0,0 +1,21 @@
+package pboif2.pkg10119052.latihan10.logika;
+
+/**
+ *
+ * NAMA   : Rizki Dwi nugraha
+ * KELAS  : IF-2
+ * NIM    : 10119052
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * operasi logika
+ * 
+ */
+public class PBOIF210119052Latihan10Logika {
+
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
